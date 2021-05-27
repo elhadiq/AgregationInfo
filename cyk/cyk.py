9ca489d75a9b5dc784de326028ea849c20d57034
zouhairelhadiq@gmail.com <zouhairelhadiq@gmail.com>
2021-05-28 00:11:24 +0100

    generator variables

--- cyk/cyk.py
+++ cyk/cyk.py
@@ -1,3 +1,5 @@
+import numpy as np
+from numpy.lib.twodim_base import mask_indices
 class GHC:
     def __init__(self,grammaire):
         self.Sigma=grammaire["Sigma"]
@@ -8,23 +10,36 @@ class GHC:
         return "Sigma: "+str(self.Sigma)+"\nV: "+str(self.V)+"\nP: "+str(self.P)
     def reduction(self):
         pass
-    def dropNonGeneratorVariables(self):
-        U=[self.Sigma]
-        pass
 
+    def generatorVariables(self):
+        U0=self.Sigma
+        U1=U0[:]
+        while True:
+            U0=U1[:]
+            mask=list(map(lambda V:any(list(map(lambda word:GHC.isIn(word,U0),self.P[V]))),self.P))
+            ##mask is a list of boolean values to indecate whether a V=P.key can generate a word in U0 or not
+            U1=U0+list(np.array(list(self.P.keys()))[mask])
+            U1=list(set(U1))
+            if U1==U0:
+                break
+        newV=list(np.intersect1d(U1,self.V))
+        return newV
     @staticmethod
-    def isIn(word,Alphabet):
+    def isIn(w,Alphabet):
+        #w : word or character, or list of words 
+        #if w is a list of words:  each word will be considred as a sngle alphabet
+        #if w is a single word : each character will be considred separently
         #return true if the word is in Alphabet false otherwise
-        return all(list(map(lambda x:x in Alphabet,set(word))))
+        return all(list(map(lambda x:x in Alphabet,set(w))))
 grammaire={   
 "Sigma":["a","b"],
 "V":["S","X","C","D"],
 "P":{
-    "S":["e","aSb"],
-    "X":["aXb","e"],
+    "S":["ab","aSb"],
+    "X":["aXb","ab"],
     "C":["D"]
 },
 "S0":"S"
 }
 G=GHC(grammaire)
-print(G)
+print(G.onlyGeneratorVariables())
