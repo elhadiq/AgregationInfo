c0fffc5e058acac1d7511bcbe6de4013b790a75c
zouhairelhadiq@gmail.com <zouhairelhadiq@gmail.com>
2021-05-28 03:12:55 +0100

    reducer function is ready

--- cyk/cyk.py
+++ cyk/cyk.py
@@ -1,5 +1,5 @@
+from functools import singledispatch
 import numpy as np
-from numpy.lib.twodim_base import mask_indices
 class GHC:
     def __init__(self,grammaire):
         self.Sigma=grammaire["Sigma"]
@@ -9,20 +9,28 @@ class GHC:
     def __repr__(self) -> str:
         return "Sigma: "+str(self.Sigma)+"\nV: "+str(self.V)+"\nP: "+str(self.P)
     def reduction(self):
-        pass
-
+        grammaire=dict()
+        grammaire["Sigma"]=self.Sigma
+        grammaire["S0"]=self.S0
+        grammaire["V"]=self.accessiblesVariables().intersection(self.generatorVariables())
+        grammaire["P"]=dict()
+        acceptedAlphabet=self.Sigma.union(grammaire["V"])
+        for Var in self.P:
+            destinations={word if GHC.isIn(word,acceptedAlphabet) else 1 for word in self.P[Var]}
+            destinations.remove(1)
+            if (len(destinations)):
+                grammaire["P"][Var]=destinations
+        return GHC(grammaire)
     def generatorVariables(self):
         U0=self.Sigma
-        U1=U0[:]
-        while True:
-            U0=U1[:]
-            mask=list(map(lambda V:any(list(map(lambda word:GHC.isIn(word,U0),self.P[V]))),self.P))
+        U1=set()
+        while U0!=U1:
+            U0=U1 if U1!=set() else U0
+            Vars=np.array(list(self.P.keys()))
+            filter=list(map(lambda V:any(list(map(lambda word:GHC.isIn(word,U0),self.P[V]))),Vars))
             ##mask is a list of boolean values to indecate whether a V=P.key can generate a word in U0 or not
-            U1=U0+list(np.array(list(self.P.keys()))[mask])
-            U1=list(set(U1))
-            if U1==U0:
-                break
-        newV=list(np.intersect1d(U1,self.V))
+            U1=U0.union(set(Vars[filter]))
+        newV=U1.intersection(self.V)
         return newV
     @staticmethod
     def isIn(w,Alphabet):
@@ -31,15 +39,42 @@ class GHC:
         #if w is a single word : each character will be considred separently
         #return true if the word is in Alphabet false otherwise
         return all(list(map(lambda x:x in Alphabet,set(w))))
-grammaire={   
-"Sigma":["a","b"],
-"V":["S","X","C","D"],
+    @staticmethod
+    def unionReducer(listOfSets):
+        u0=set()
+        for u in listOfSets:
+            u0=u0.union(u)
+        return u0
+    def accessiblesVariables(self):
+        W0={self.S0}
+        W1=set()
+        while W1!=W0:
+            W0=W1 if W1!=set() else W0
+            rulesfromW0="".join(GHC.unionReducer([self.P[Var] for Var in W0.intersection(list(self.P.keys()))]))
+            #now we have to path in order to menimase: if the alphabet Sigma is so small than the set V we can remove 
+            # the alphabet character by charater until having only reachable variables of V
+            # OR if the V is small we can test for each var in V if is it reacheable or not
+            newAccesibles=set()
+            if len(self.V)<len(self.Sigma):
+                    newAccesibles={Var if Var in rulesfromW0 else "Nan" for Var in self.V}
+                    newAccesibles.remove("Nan")
+            else:
+                for a in self.Sigma:
+                    rulesfromW0=rulesfromW0.replace(a,'')
+                newAccesibles=set(list(rulesfromW0))
+            W1=W1.union(newAccesibles)
+        return W1    
+grammaire={
+"Sigma":{"a","b"},
+"V":{"S","X","C","D"},
 "P":{
-    "S":["ab","aSb"],
-    "X":["aXb","ab"],
-    "C":["D"]
+    "S":{"ab","aSb","X"},
+    "X":{"aXb","abC"},
+    "C":{"D"}
 },
 "S0":"S"
 }
 G=GHC(grammaire)
-print(G.onlyGeneratorVariables())
+print("generators: ",G.generatorVariables())
+print("Accecibles: ",G.accessiblesVariables())
+print("new",G.reduction())
