9d35fe5c4b3798003d0a08ff95373697be6ceaaf
zouhairelhadiq@gmail.com <zouhairelhadiq@gmail.com>
2021-06-05 09:46:43 +0100

    Fixing list deep copy in computing nCp iteratevly

--- Algorithms/PDBUCombinaison.py
+++ Algorithms/PDBUCombinaison.py
@@ -1,9 +1,7 @@
 def PDBUCombinaison(n,p): 
-    Track=[[0]*(p+1)]*(n+1)
-    print("Tableau avant:",Track)
+    Track=[[0 for _ in range(p+1)] for _ in range(n+1)]
     for i in range (p+1): 
         Track[0][i]=0 
-    # Track[0][]
     for i in range (1,n+1):
         for j in range(p+1): 
             if j==0 or j== i:
@@ -11,8 +9,7 @@ def PDBUCombinaison(n,p):
             elif j==1 or j== i-1:
                 Track[i][j]=i
             else:
-                Track[i][j]=Track[i-1][j-1]+Track[i-1][j]
-    print("Tableau apres:",Track)            
+                Track[i][j]=Track[i-1][j-1]+Track[i-1][j]           
     return Track[n][p]
 
 def PDTDRecCombinaison(n,p,T): 
@@ -22,8 +19,6 @@ def PDTDRecCombinaison(n,p,T):
     elif p==1 or p==n-1 : 
        T[p][n]=n
        return n
-    # elif T[p][n]>0:
-        # return T[p][n]
     else:
         T[p][n]=PDTDRecCombinaison(n-1,p-1,T)+PDTDRecCombinaison(n-1,p,T)
         return T[p][n]
