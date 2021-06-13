38c9b5cfb55484219dc6c395e46c570d92642784
zouhairelhadiq@gmail.com <zouhairelhadiq@gmail.com>
2021-06-13 01:04:24 +0100

    new algos

---
+++ Algorithms/suduku.py
@@ -0,0 +1,24 @@
+import numpy as np
+grille=np.array( [
+[0,0,6,8,5,0,0,0,0,],
+[0,2,0,0,0,0,1,0,0,],
+[4,0,0,2,1,0,3,0,8,],
+[8,9,0,4,0,0,5,0,6,],
+[7,3,0,1,0,8,0,9,2,],
+[2,0,4,0,0,5,0,7,1,],
+[9,0,2,0,8,6,0,0,3,],
+[0,0,3,0,0,0,0,8,0,],
+[0,0,0,0,3,4,2,0,0,],
+])
+a=np.matrixlib.asmatrix(grille)
+def resoudre(grille):
+    DoneLigne=[]
+    DoneColone=[]
+    DoneBlock=[]
+def resoutLigne(i):
+    pass
+def resoutColone(j):
+    pass
+def resoutBlock(c):
+    pass
+print(a)
