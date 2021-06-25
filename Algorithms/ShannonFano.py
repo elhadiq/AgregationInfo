06a34a1a5c0a722d651f6dba0e2d087952959d79
El Hadiq Zouhair <zouhairelhadiq@gmail.com>
2021-06-25 13:55:17 +0100

    Shannon Dano rectif

--- Algorithms/ShannonFano.py
+++ Algorithms/ShannonFano.py
@@ -1,18 +1,26 @@
-message="INFORMATION RESEAUX ET COMMUNICATIONS"
 def compterOccurences(message):
     #enlever les espaces
-    msg="".join(message.split(' '))
     NbOccurences=dict()
-    for c in msg:
+    for c in message:
         try:
             NbOccurences[c]+=1
         except:
             NbOccurences[c]=1
     return NbOccurences
+
+
 trierParOccurences =lambda NbOccurences:sorted(NbOccurences.items(), key=lambda tup: tup[1])
+
+
 initialisationsCodes=lambda NbOccurences:{A:"" for A in NbOccurences}
+
+
 getWieght=lambda sortedList:sum([s[1] for s in sortedList])
+
+
 diffWeigth=lambda gauche,droite:abs(getWieght(gauche)-getWieght(droite))
+
+
 def divide(sortedList):
     assert len(sortedList)>1, "i cant divide this list"
     if len(sortedList)==2:
@@ -28,6 +36,7 @@ def divide(sortedList):
             gauch,droite=tG,tD
     return gauch,droite
 
+
 def affectBits(srtList,codageDict):
     if len(srtList)<=1:
         pass
@@ -39,23 +48,26 @@ def affectBits(srtList,codageDict):
             codageDict[C]=codageDict[C]+"1"
         affectBits(d,codageDict)
         affectBits(g,codageDict)
+
+
 def shanonFanon(message):
     NbOccurences=compterOccurences(message)
     srt=trierParOccurences(NbOccurences)
     codageDict=initialisationsCodes(NbOccurences)
     affectBits(srt,codageDict)
-    codedMessage="".join([codageDict[C] if C!=" " else " " for C in message])
+    codedMessage="".join([codageDict[C] for C in message])
     return codedMessage,codageDict
+
+
 def decoderShanonFanon(codedMessage,codageDict):
     message=""
     bande=codedMessage[:]
     decodeDict={codageDict[C]:C for C in codageDict}
-    decodeDict[" "]=" "
     i=0
-    while i<len(bande)>1:
+    while i<=len(bande)>0:
         i=1
         notfound=True
-        while notfound and i<len(bande):
+        while notfound and i<=len(bande):
             code=bande[:i]
             try:
                 decode=decodeDict[code]
@@ -65,5 +77,8 @@ def decoderShanonFanon(codedMessage,codageDict):
         message+=decode
         bande=bande[i:]
     return message
-codedMessage,codageDict=shanonFanon(message)
-print(decoderShanonFanon(codedMessage,codageDict))
+
+if __name__=='__main__':
+    message="INFORMATION RESEAUX ET COMMUNICATIONS"
+    codedMessage,codageDict=shanonFanon(message)
+    print(decoderShanonFanon(codedMessage,codageDict))
