76b7010d9ca86f0a37f4d078f27693108490f3d3
Zouhair El Hadiq <zouhairelhadiq@gmail.com>
2021-07-05 13:16:06 +0100

    Recursive Dynamic Coin changing sollution

--- Algorithms/Dynamicprogramming.py
+++ Algorithms/Dynamicprogramming.py
@@ -31,12 +31,35 @@ def coinChangingNaiveDynamique(Coins,Somme,cash=dict(),i=None):
     cash[Somme]=nb
     return nb
 
+def coinChangingNaiveDynamiqueSollutions(Coins,Somme,cash=dict(),i=None):
+    if i is None:
+        i=len(Coins)-1
+    if Somme in cash:
+        return cash[Somme]
+    if Somme<=0:
+        if Somme==0:
+            cash[Somme]=[0,[]]
+            return cash[Somme]
+        else:
+            cash[Somme]=[math.inf,[]]
+            return [math.inf,[]]
+    if i==0:
+        nb=[1,[Coins[0]]] if Somme==Coins[0] else [math.inf,[]]
+    else:
+        path1=coinChangingNaiveDynamiqueSollutions(Coins,Somme,cash,i-1)
+        path2=coinChangingNaiveDynamiqueSollutions(Coins,Somme-Coins[i],cash,i)
+        path2[0]=path2[0]+1
+        path2[1]=path2[1]+[Coins[i]]
+        nb=path1 if path1[0]<path2[0] else path2
+    cash[Somme]=nb
+    return nb
 
 Coins=[4,3,1]
-Somme=600
-now=time.time()
-print(coinChangingNaive(Coins,Somme))
-print("time naive=",time.time()-now)
-now=time.time()
-print(coinChangingNaiveDynamique(Coins,Somme))
-print("time dynamique=",time.time()-now)
+Somme=10
+# now=time.time()
+# print(coinChangingNaive(Coins,Somme))
+# print("time naive=",time.time()-now)
+# now=time.time()
+# print(coinChangingNaiveDynamique(Coins,Somme))
+# print("time dynamique=",time.time()-now)
+print(coinChangingNaiveDynamiqueSollutions(Coins,Somme))
